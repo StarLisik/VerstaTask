6acca635be2d390cd2d0f04668acaf55ef3aafd7
Родион Бурмистров <rodion3000000@gmail.com>
2026-09-20 18:52:46 +0300

    added misssing files

---
+++ Migrations/20260920141002_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VerstaTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CitySender = table.Column<string>(type: "TEXT", nullable: false),
+                    AdressSender = table.Column<string>(type: "TEXT", nullable: false),
+                    CityReceiver = table.Column<string>(type: "TEXT", nullable: false),
+                    AdressReceiver = table.Column<string>(type: "TEXT", nullable: false),
+                    Weight = table.Column<decimal>(type: "TEXT", nullable: false),
+                    PickDate = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
